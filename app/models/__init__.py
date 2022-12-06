cc1c0cf3ca3fc34fbebbfb76cdd801bea946c754
Dmitry <kuznetsov@logwest.ru>
2022-12-06 23:04:59 +0300

    Sprint 23. Try 3.

--- app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .charity_project import CharityProject # noqa
-from .user import User # noqa
-from .donation import Donation # noqa
--- app/models/donation.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, Text
-
-from app.core.db import Base
-
-
-class Donation(Base):
-    user_id = Column(Integer, ForeignKey('user.id'))
-    comment = Column(Text)
