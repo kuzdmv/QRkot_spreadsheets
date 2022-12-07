167a6b68e17a737f84e33e3ce4048377fb6e70cb
Dmitry <kuznetsov@logwest.ru>
2022-12-07 18:16:49 +0300

    Sprint 23. Try 6.

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -8,14 +8,20 @@ from app.models.charity_project import CharityProject
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 SPREADSHEET_BODY = {
-        'properties': {'title': 'Отчет на {now_date_time}',
-                       'locale': 'ru_RU'},
-        'sheets': [{'properties': {'sheetType': 'GRID',
-                                   'sheetId': 0,
-                                   'title': 'Лист1',
-                                   'gridProperties': {'rowCount': 100,
-                                                      'columnCount': 11}}}]
-    }
+    'properties': {
+        'title': 'Отчет на {now_date_time}',
+        'locale': 'ru_RU'
+    },
+    'sheets': [{'properties': {
+        'sheetType': 'GRID',
+        'sheetId': 0,
+        'title': 'Лист1',
+        'gridProperties': {
+            'rowCount': 100,
+            'columnCount': 11
+        }
+    }}]
+}
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
